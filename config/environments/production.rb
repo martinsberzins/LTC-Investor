245cd6b353307505c5d390367f24de013f5df44a
Martins Berzins <martins_berzins@inbox.lv>
2014-07-18 09:02:26 +0000

    ch2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,10 +76,10 @@ Project::Application.configure do
   # config.autoflush_log = false
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::Logger::Formatter.
+  config.log_formatter = ::Logger::Formatter.new
 
   #for confirmation emails
-  #config.action_mailer.default_url_options = { :host => 'ltc-investor.herokuapp.com' }
-  #config.action_mailer.delivery_method = :smtp
-  #config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { :host => 'ltc-investor.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
 end
