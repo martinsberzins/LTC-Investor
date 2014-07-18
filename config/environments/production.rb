49baaafeca4b610814ea4a2252de60028bb6514d
Martins Berzins <martins_berzins@inbox.lv>
2014-07-18 08:52:35 +0000

    added confirmation email for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,5 +76,10 @@ Project::Application.configure do
   # config.autoflush_log = false
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::Logger::Formatter.new
+  config.log_formatter = ::Logger::Formatter.
+
+  #for confirmation emails
+  config.action_mailer.default_url_options = { :host => 'ltc-investor.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
 end
