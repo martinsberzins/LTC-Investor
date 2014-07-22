52a158a8b9c9535c38d893f1d9ab5129c2d7f39b
Martins Berzins <martins_berzins@inbox.lv>
2014-07-22 20:56:44 +0000

    hover on divs

--- app/assets/javascripts/posts_hover_div.js
+++ app/assets/javascripts/posts_hover_div.js
@@ -1,13 +1,11 @@
 var ready;
 ready = function(){
-	$(".posts_index_hover_div").hide()
-	$(".posts_index_div").hover(function(){ 
-        $(".posts_index_hover_div").show("slide", { direction: "down" }, 1000);, 
-        function(){
-        $(".posts_index_hover_div").hide("slide", { direction: "down" }, 1000);;
-        console.log("hover");
-      	});
-}	
+	$(".posts_index_hover_div").hide();
+	$( ".posts_index_div")
+  		.hover(function() {
+    $(this).find(".posts_index_hover_div").stop().slideToggle();
+  	});
+	};	
 
 
 $(document).ready(ready);
